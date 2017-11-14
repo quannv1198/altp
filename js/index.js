58e141d101b927e60e5b3b83c1124ffa6d432007
quannv <quannv1198@gmail.com>
2017-11-14 22:29:27 +0700

    ai la trieu phu

---
+++ js/index.js
@@ -0,0 +1,347 @@
+window.game={
+	answered:['Quan','IT'],
+	bank:[
+		{
+			question:'Đại Ngu là quốc hiệu của triều đại nào?',
+			answer:['Triều Hồ','Triều Ngô','Nhà Tây Sơn','Các Chúa Nguyễn']
+		},
+		{
+			question:'Các vua Hùng đặt quốc hiệu nước ta là gì?',
+			answer:['Văn Lang','Âu lạc','Vạn Xuân','Đại Việt']
+		},
+		{
+			question:'Sông Bến Hải và sông Thạch Hãn nằm ở tỉnh nào?',
+			answer:['Quảng trị','Quảng Bình','Quảng Ninh','Quảng Ngãi']
+		},
+		{
+			question:'Trong các cây cầu sau, cầu nào là cầu xoay?',
+			answer:['Cầu Sông Hàn','Cầu Thanh Trì','Cầu Cần Thơ','Cầu Thị Nại']
+		},
+		{
+			question:'An Dương Vương đặt quốc hiệu nước ta là gì?',
+			answer:['Âu lạc','Văn Lang','Vạn Xuân','Đại Việt']
+		},
+		{
+			question:'Năm 1010, Lý Thái Tổ đã cho xây dựng điện nào ở trung tâm hoàng thành Thăng Long?',
+			answer:['Điện Càn Nguyên','Điện Kính Thiên','Điện Càn Chánh','Điện Càn Thành']
+		},
+		{
+			question:'"Điêu tàn" là tập thơ đầu tiên của nhà thơ nào?',
+			answer:['Chế Lan Viên','Anh Thơ','Thế Lữ','Bính Khê']
+		},
+		{
+			question:'Cúm và sởi là những bệnh lây truyền qua đường nào?',
+			answer:['Hô hấp','Tiêu hóa','Sinh dục','Máu']
+		},
+		{
+			question:'Nước nào ở khu vực Đông Nam Á không có địa giới với bất kỳ nước khác?',
+			answer:['Philippines','Malaysia','Lào','Myanma']
+		},
+		{
+			question:'Tượng đài Mẹ Suốt nằm bên cạnh con sông nào?',
+			answer:['Sông Nhật Lệ','Sông Giang','Sông Long Đại','Sông Kiến Giang']
+		},
+		{
+			question:'Giáo sư Trần Văn Khê nổi tiếng ở lĩnh vực nào?',
+			answer:['Âm nhạc','Toán học','Thiên văn học','Lịch sử']
+		},
+		{
+			question:'Paracetamol thuộc nhóm thuốc nào sau đây?',
+			answer:['Thuốc hạ sốt, giảm đau','Thuốc lợi tiểu','Thuốc điều trị tăng huyết áp','Thuốc an thần, bình thân']
+		},
+		{
+			question:'Điền từ còn thiếu trong câu thành ngữ:’”…. Không cứu được …gần”.',
+			answer:['Nước-Lửa','Chó-mèo','Lính-tướng','Chồng-vợ']
+		},
+		{
+			question:'Tập hợp các số thực được ký hiệu bằng chữ cái nào?',
+			answer:['R','Z','N','Q']
+		},
+		{
+			question:'Truyện “Vợ chồng A Phủ” trong tập truyện Tây Bắc của nhà văn Tô Hoài viết về người dân tộc nào?',
+			answer:['H\'Mông','Tày','Thái','Vân Kiều']
+		},
+		{
+			question:'Côn trùng (sâu bọ) là lớp động vật thuộc ngành động vật nào?',
+			answer:["Thân mềm",'Ruột khoang','Chân khớp','Động vật nguyên sinh']
+		},
+		{
+			question:'Ngọn núi nào được nhắc tới trong bài thơ "Quê mẹ" của Tố Hữu?',
+			answer:['Thiên Khai','Ngự Bình','Non Nước','Hông Lĩnh']
+		},
+		{
+			question:'Địa danh nào còn thiếu trong câu ca dao : ....có núi Vọng Phu Có đầm Thị Nại có cù lao Xanh"?',
+			answer:['Bình Định','Phú yên','Long An','Khánh Hòa']
+		},
+		{
+			question:'"Cỏ cú" là tên gọi khác của loại cỏ nào dưới đây?',
+			answer:['Cỏ gấu','Cỏ gừng','Cỏ chanh','Cỏ may']
+		},
+		{
+			question:' Vị tướng nào dưới thời Hai Bà Trưng là ông tổ của lò vật Mai Động?',
+			answer:['Tam Trinh',"Ông Đông Bảng",'Ông Đồng','Đào Kì']
+		},
+		{
+			question:'Bối cảnh của câu chuyện tỉnh Romeo và Juliet là thành phố nào của nước Ý?',
+			answer:['Verona','Venice','Milan','Rome']
+		},
+		{
+			question:'Giải thưởng Emmy là giải thưởng cao quý của thể loại giải trí nào?',
+			answer:['Truyền hình','Điện Ảnh','Âm nhạc','Thể thao']
+		},
+		{
+			question:'“Vội vã trở về, vội vã ra đi …” là những dòng trăn trở của nhạc sĩ Phú Quang trong bài hát Hà Nội ngày trở về. Trong ca khúc này, nhạc sĩ Phú Quang đã so sánh nỗi nhớ với dòng sông nào?',
+			answer:['Sông Hồng','Sông Cửu Long','Sông Lô','Sông Mã']
+		},
+		{
+			question:'Trong dấu ba chấm là địa danh nào: "....có suối đãi vàng Có hồ Ba Bể có nàng áo xanh"?',
+			answer:['Bắc Cạn','Thái Nguyên','Cao Bằng','lạng Sơn']
+		},
+		{
+			question:'Tượng đài chiến thắng Điện Biên Phủ được dựng ở ngọn đồi nào?',
+			answer:['D1','A1','B1','C1']
+		},
+		{
+			question:'Nhất Trí, nhì Lân, tam Vân, tứ Cẩn là chỉ bộ tứ đại thụ ở bộ môn nghệ thuật nào?',
+			answer:['Hộ Họa','Âm nhạc','Sân Khấu','Điện ảnh']
+		},
+		{
+			question:' Ure là loại phân nào?',
+			answer:['Phân đạm','Phân lân','Phân kali','Phân NPK']
+		},
+		{
+			question:'Bài hát “Hồn sĩ tử” của nhạc sĩ Lưu Hữu Phước được dùng trong các lễ tang còn có tên gọi là “Hát Giang trường hận” được lấy cảm hứng từ cuộc khởi nghĩa nào?',
+			answer:['Khởi nghĩa Hai Bà Trưng','Khởi Nghĩa Bà Triệu','Khởi nghĩa Mai Trúc Loan','Khởi nghĩa Phùng Hưng']
+		},
+		{
+			question:'Hành tinh nào trong Hệ Mặt Trời có chu kỳ tự quay quanh trục và độ nghiêng gần giống Trái Đất nhất?',
+			answer:['Sao hỏa','Sao Thủy','Sao Mộc','Sao kim']
+		},
+		{
+			question:'Đâu là tác phẩm được viết và đóng thành quyển sách nặng nhất ở Việt Nam do nhà thư pháp Nguyệt Đình thực hiện.',
+			answer:['Truyện Kiều','Bình Ngô đại cáo','Truyện Lục Vân Tiên','Đại từ điển tiềng Việt']
+		},
+		{
+			question:'Châu lục nào tiếp giáp với cả Đại Tây Dương và Ấn Độ Dương?',
+			answer:['Châu Phi','Châu Á','Châu Mỹ','Châu Âu']
+		},
+		{
+			question:'Cờ đỏ sao vàng lần đầu tiên xuất hiện tại cuộc khởi nghĩa nào?',
+			answer:['Khởi nghĩa Nam kì','Khởi nghĩa Bắc Sơn','Binh biến Đô Lương','Khởi nghĩa Ba Tơ']
+		}
+	],
+	money:[
+		"0",
+		'0',
+		'200',
+		'400',
+		'600',
+		'1.000',
+		'2.000',
+		'3.000',
+		'6.000',
+		'10.000',
+		'14.000',
+		'22.000',
+		'30.000',
+		'40.000',
+		'60.000',
+		'85.000',
+		'150.000'
+	],
+	audioStart: new Audio("audio/start.mp3"),
+	audio: new Audio("audio/audio4.mp3"),
+	audioTrue: new Audio("audio/true.mp3"),
+	audioFalse: new Audio("audio/false.mp3"),
+	ar:'N',
+	checked:true,
+	index:0,
+	time:0,
+	question:1,
+	timeOut: function(){
+		setInterval(function(){
+			if(game.time<0){
+				$('#time').text('.');	
+			}else{
+				if(game.time==0){
+					game.time=-1;
+					game.checked=true;
+					var r = $('[value="'+game.ar+'"]');
+					var b = 0;
+					game.audio.pause();
+					game.audioFalse.load();
+					game.audioFalse.play();
+					var a =setInterval(function(){
+							if(b == 0){
+								$(r).css("background","yellow");
+								b=1;
+							}
+							else{
+								$(r).css("background","#1E88E5");
+								b=0;
+							}
+						 },200);
+					setTimeout(function(){
+						clearInterval(a);
+						gameOver();
+					},2000);
+				}
+				else{
+					game.time--;
+					$('#time').text(game.time);
+				}
+			}
+
+		},1000);
+	},
+	play: function(){
+		$('#money').show();
+		game.audio.volume = 1;
+		game.audio.load();
+		game.audio.play();
+		if(game.question==16){
+			$('#question').text('Win');
+			$('#content').text('Chúc mừng bạn đã trở thành triệu phú với 150.000.000 VNĐ tiền thưởng.');
+			game.question=1;
+		}
+		game.checked = false;
+		$('#answer').each(function(i,v){
+			$(this).css("background","#1E88E5");
+		});
+		game.time = 26;
+		if(game.answered.length==game.bank.length+2){
+			for(var i = game.answered.length-1;i>1;i--){
+				game.answered.pop();
+			}
+		}
+		$('#question').text('Câu hỏi số '+game.question);
+		$('#money').text(game.money[game.question]);
+		var check = false;
+		var c;
+		while(check==false){
+			c = Math.floor(Math.random()*game.bank.length);
+			var check2 = true;
+			for(var i = 0;i<game.answered.length;i++){
+				if(c==game.answered[i]){
+					check2 = false;
+				}
+			}
+			if(check2 == true){
+				check = true;
+			}
+		}
+		game.answered.push(c);
+		game.index = c;
+		var data = game.bank[c];
+		$('#content').text(data.question);
+		var ran = ['data','data','data','data'];
+		ran[0] = data.answer[0];
+		ran[1] = data.answer[1];
+		ran[2] = data.answer[2];
+		ran[3] = data.answer[3];
+		game.ar = 'A';
+		var b = Math.floor(Math.random()*3)+1;
+		if(b==1){
+			game.ar = 'B';
+		}
+		if(b==2){
+			game.ar = 'C';
+		}
+		if(b==3){
+			game.ar = 'D';
+		}
+		var temp = ran[b];
+		ran[b]=ran[0];
+		ran[0]=temp;
+		$('[value="A"]').text('A: '+ran[0]);
+		$('[value="B"]').text('B: '+ran[1]);
+		$('[value="C"]').text('C: '+ran[2]);
+		$('[value="D"]').text('D: '+ran[3]);
+	}
+}
+$(document).ready(function(){
+	$('#money').hide();
+	game.audioStart.play();
+});
+
+function checkAnswer(e){
+	if(game.checked==false){
+		game.audio.volume = 0.5;
+		game.time=-1;
+		game.checked = true;
+		var result = $(e).text();
+		$(e).css("background","green");
+		result = result.slice(3,result.length);
+		if(result==game.bank[game.index].answer[0]){
+			game.question++;
+			setTimeout(function(){
+				var b = 0;
+				game.audioTrue.load();
+				game.audioTrue.play();
+				var a =setInterval(function(){
+						if(b == 0){
+							$(e).css("background","yellow");
+							b=1;
+						}
+						else{
+							$(e).css("background","green");
+							b=0;
+						}
+					 },200);
+				setTimeout(function(){
+					clearInterval(a);
+					$(e).css("background","#1E88E5");
+					game.play();
+				},2000);
+			},2000);
+		}
+		else{
+			var r = $('[value="'+game.ar+'"]');
+			setTimeout(function(){
+				var b = 0;
+				game.audioFalse.load();
+				game.audioFalse.play();
+				var a =setInterval(function(){
+						if(b == 0){
+							$(r).css("background","yellow");
+							b=1;
+						}
+						else{
+							$(r).css("background","#1E88E5");
+							b=0;
+						}
+					 },200);
+				setTimeout(function(){
+					clearInterval(a);
+					$(e).css("background","#1E88E5");
+					gameOver();
+				},2000);
+			},2000);
+		}
+	}
+}
+function gameOver(){
+	$('[value="'+game.ar+'"]').css("background","#1E88E5");
+	$('#money').hide();
+	$('[onclick="checkAnswer(this)"]').each(function(){
+		$(this).text(".");
+	});
+	$("#answer").text('Chơi lại');
+	$("#infor").text('Thông tin');
+	$("#answer").attr('onclick','startGame()');
+	$("#infor").attr('onclick','infor()');
+	$('#question').text('Game over');
+	$('#content').text("Chúc mừng bạn đã ra về với "+game.money[game.question]+".000 VNĐ tiền thưởng");
+	game.question=1;
+}
+function startGame(){
+	game.audioStart.pause();
+	$("#answer").attr('onclick','checkAnswer(this)');
+	$("#infor").attr('onclick','checkAnswer(this)');
+	game.play();
+	game.timeOut();
+}
+function infor(){
+	$('#question').text('Thông Tin');
+	$('#content').html('<br>Game: Ai là triệu phú<br>Được tạo bởi: QuanIT<br>Gmail: <a href="">Laptrinhvient3h@gmail.com</a><br>Facebook: <a href="">Quan Nguyen</a>');
+}
